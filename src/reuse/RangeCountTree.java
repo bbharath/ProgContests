683c26304901421cfda5d96e3670b5a9cdf4b4d2
Bharath Balakrishnan <bharath.falcon@gmail.com>
2014-08-20 19:47:11 +0530

    [Topcoder] SRM 627 Div 1 500

--- src/reuse/RangeCountTree.java
+++ src/reuse/RangeCountTree.java
@@ -3,9 +3,9 @@ package reuse;
 import java.util.Arrays;
 
 /*
- * RangeCountTree - a tree that implements range queries. Also known as a BinaryIndexedTree
- * Insert and Delete are O(N);
- * Takes space (2*X+1 where X = 2^y where y is smallest number : such that X = 2^y >=N)
+ * RangeCountTree - a tree that implements range queries. Also known as a BinaryIndexedTreegi
+ * Insert and Delete are O(log N);
+ * Takes space (2*X+1 where X = 2^y where y is an integer, X is smallest number : such that X = 2^y >=N)
  */
 public class RangeCountTree {
 	int[] counts;
